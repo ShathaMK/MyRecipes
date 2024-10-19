0a54d2beb8da5375fe0832425f17e1c9531e96e3
ShathaMK <shathamukhaild@gmail.com>
2024-10-19 20:30:12 +0300

    Initial Commit

---
+++ MyRecipes/MyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipesApp.swift
+//  MyRecipes
+//
+//  Created by Shatha Almukhaild on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

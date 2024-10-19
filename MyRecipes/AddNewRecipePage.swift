5e7215ec9a05f7bbc5873d8500328f7359bce783
ShathaMK <shathamukhaild@gmail.com>
2024-10-20 00:26:43 +0300

    Created main page and Add new Recipe page linked both pages using navigation stack

---
+++ MyRecipes/AddNewRecipePage.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewRecipePage.swift
+//  MyRecipes
+//
+//  Created by Shatha Almukhaild on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+struct AddNewRecipePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewRecipePage()
+}

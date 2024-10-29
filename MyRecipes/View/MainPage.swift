8b46398dacf7ff76740e4842794fda9c47a645d3
ShathaMK <shathamukhaild@gmail.com>
2024-10-29 04:26:38 +0300

    Search bar in main page now works

--- MyRecipes/View/MainPage.swift
+++ MyRecipes/View/MainPage.swift
@@ -12,97 +12,134 @@ struct MainPage: View {
     // used to check if there is a recipe and change the view based on that
     @ObservedObject var viewModel = RecipeViewModel()
     @State var navigateToView = false
+    @State private var searchText: String = ""
+    var filteredRecipes: [Recipe] {
+         if searchText.isEmpty {
+             return viewModel.recipes
+         } else {
+             return viewModel.recipes.filter { recipe in
+                 recipe.recipeTitle.localizedCaseInsensitiveContains(searchText)
+             }
+         }
+     }
 
     var body: some View {
 
         NavigationStack{
-            ScrollView{
-                //list recipes if its not empty
-                if(!viewModel.recipes.isEmpty){
-                    ForEach(viewModel.recipes) { recipe in
-                    Spacer()
-                    VStack(alignment: .center){
-                        
-                        ZStack(alignment: .leading){
-                            // Safely unwrap the image
-                            if let image = recipe.recipeImage {
-                                Image(uiImage: image)
-                                    .resizable()
-                                    .frame(maxWidth: UIScreen.screenWidth, maxHeight: 272, alignment: .leading)
-                                    } else {
-                                    // Placeholder image if the recipe image is nil
-                                       Image("SpoonNFork")
+            ScrollView {
+                VStack {
+                   //
+                    //list recipes if its not empty
+                    if(!filteredRecipes.isEmpty){
+                        // Search Bar
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        ZStack{
+                           //
+                            TextField("     Search", text: $searchText)
+                                .padding()
+                                .background(Color(.systemGray6))
+                                .cornerRadius(8)
+                                .frame(width: 380,height: 30)
+                               // .padding(.horizontal)
+                            // Magnifying glass icon
+                            
+                           
+                              Image(systemName: "magnifyingglass")
+                                  .foregroundColor(.gray)
+                                  .padding(.trailing,330 ) // Position the icon
+                            
+                            Image(systemName: "mic.fill")
+                                .foregroundColor(.gray)
+                                .padding(.leading,330 )
+                        }
+                        ForEach(filteredRecipes) { recipe in
+                            Spacer()
+                            VStack(alignment: .center){
+                                
+                                ZStack(alignment: .leading){
+                                    // Safely unwrap the image
+                                    if let image = recipe.recipeImage {
+                                        Image(uiImage: image)
                                             .resizable()
                                             .frame(maxWidth: UIScreen.screenWidth, maxHeight: 272, alignment: .leading)
-                                         }
-                            
-                               // Gradient shadow
-                            LinearGradient(gradient: Gradient(colors: [Color.clear,Color.black.opacity(0.7)]), startPoint: .top, endPoint: .bottom)
-                                .frame(width: UIScreen.screenWidth, height: 272)
-                                              
-
-                                
-                            Text("\(recipe.recipeTitle)").font(.title).bold().foregroundStyle(.white)
-                                .padding(.top,75)
-                                .padding(.leading,20)
-                                .padding(.bottom)
-                            
-                            VStack (alignment: .leading){
-                                Text("\(recipe.Description)").font(.footnote).lineLimit(2)
+                                    } else {
+                                        // Placeholder image if the recipe image is nil
+                                        Image("SpoonNFork")
+                                            .resizable()
+                                            .frame(maxWidth: UIScreen.screenWidth, maxHeight: 272, alignment: .leading)
+                                    }
                                     
-                                    .frame(maxWidth: 360, alignment: .leading)
-                                    .foregroundStyle(.white)
-                                    .padding(.top,200).padding(.leading,25)
-                                //Spacer()
-                                NavigationLink(destination: ViewRecipe(viewModel: viewModel, recipeToDelete: recipe, recipe: recipe)) {
-                                    Text("See All")
-                                        .font(.footnote)
-                                        .foregroundStyle(Color("ColorOrange")).bold()
-                                        .padding(.leading, 25)
+                                    // Gradient shadow
+                                    LinearGradient(gradient: Gradient(colors: [Color.clear,Color.black.opacity(0.7)]), startPoint: .top, endPoint: .bottom)
+                                        .frame(width: UIScreen.screenWidth, height: 272)
+                                    
+                                    
+                                    
+                                    Text("\(recipe.recipeTitle)").font(.title).bold().foregroundStyle(.white)
+                                        .padding(.top,75)
+                                        .padding(.leading,20)
                                         .padding(.bottom)
-                                                        }
-                                   // .padding(.leading,130)
+                                    
+                                    VStack (alignment: .leading){
+                                        Text("\(recipe.Description)").font(.footnote).lineLimit(2)
+                                        
+                                            .frame(maxWidth: 360, alignment: .leading)
+                                            .foregroundStyle(.white)
+                                            .padding(.top,200).padding(.leading,25)
+                                        //Spacer()
+                                        NavigationLink(destination: ViewRecipe(viewModel: viewModel, recipeToDelete: recipe, recipe: recipe)) {
+                                            Text("See All")
+                                                .font(.footnote)
+                                                .foregroundStyle(Color("ColorOrange")).bold()
+                                                .padding(.leading, 25)
+                                                .padding(.bottom)
+                                        }
+                                        // .padding(.leading,130)
+                                        
+                                        Spacer()
+                                    }
+                                    
+                                    
+                                }
                                 
-                                Spacer()
+                                
+                                //                        
                             }
                             
+                        }.padding(.horizontal)
+                            .padding(.top,25)// Optional: adds padding to the VStack
+                            .navigationBarTitle("Food Recipes")//End of VStack
+                            .navigationBarBackButtonHidden(true)
+                        //
+                    } // Hide the back button when new recipe added
+                    else{
+                        VStack{
+                            Image("SpoonNFork")
+                                .resizable()
+                                .frame(width: 325,height: 327)
+                                .padding(.top,86)
                             
-                        }
-                        
-                     
-                        //                        
-                                            }
-                       
-                    }.padding(.horizontal)
-                    .padding(.top,25)// Optional: adds padding to the VStack
-                    .navigationBarTitle("Food Recipes")//End of VStack
-                    .navigationBarBackButtonHidden(true)
-                    //
-                } // Hide the back button when new recipe added
-                else{
-                    VStack{
-                        Image("SpoonNFork")
-                            .resizable()
-                            .frame(width: 325,height: 327)
-                            .padding(.top,86)
-                        
-                        /* to make the colors of the text adaptable to light and dark mode
-                         .primary is black in light mode and white in dark mode
-                         .secondary is gray in both modes */
-                        
-                        Text("There's no recipe yet")
-                            .font(.system(.largeTitle))
-                            .bold()
-                            .foregroundStyle(.primary)
-                            .padding(.top,24)
-                        Text("Please add your recipes")
-                            .font(.system(.title3))
-                            .foregroundStyle(.secondary)
-                            .padding(.top,-2)
-                    }.navigationBarTitle("Food Recipes")//End of VStack
-                     .navigationBarBackButtonHidden(true)
+                            /* to make the colors of the text adaptable to light and dark mode
+                             .primary is black in light mode and white in dark mode
+                             .secondary is gray in both modes */
+                            
+                            Text("There's no recipe yet")
+                                .font(.system(.largeTitle))
+                                .bold()
+                                .foregroundStyle(.primary)
+                                .padding(.top,24)
+                            Text("Please add your recipes")
+                                .font(.system(.title3))
+                                .foregroundStyle(.secondary)
+                                .padding(.top,-2)
+                        }.navigationBarTitle("Food Recipes")//End of VStack
+                            .navigationBarBackButtonHidden(true)
+                    }
                 }
-                }//End of ScrollView
+            }//End of ScrollView
                 .toolbar {
                                 // Adding SF Symbol as a button in the top bar
                    
